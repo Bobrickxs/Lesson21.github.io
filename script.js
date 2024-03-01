492eae8aa7db89ed0e99068568291dab5bf601bf
KsiuTretyakova <hrytsenkoks@gmail.com>
2024-03-01 18:11:12 +0200

    Реалізація малювання кола за кліком

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function draw(event) {
+// function draw() {
     let canvas = document.getElementById("imgCanvas");
+    let ctx = canvas.getContext("2d");
+    let rect = canvas.getBoundingClientRect();
+
+    let posX = event.clientX - rect.left;
+    let posY = event.clientY - rect.top;
+
+    ctx.fillStyle = "#00ff00";
+
+    ctx.beginPath();
+
+    ctx.arc(posX, posY, 50, 0, 2 * Math.PI);
+
+    ctx.fill()
+
+    
 }
+
+imgCanvas.onclick = draw;
